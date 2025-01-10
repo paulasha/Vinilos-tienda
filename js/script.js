76bca00ef80f7dcb6311f0fd621387242e746b8d
paulasha <paula.cuadros.b@gmail.com>
2025-01-10 01:08:47 +0100

    CONEXION DE INDEX CONCAFETERÍA

--- js/script.js
+++ js/script.js
@@ -240,63 +240,7 @@ function playAnimation() {
   });
   
   
-// botón flecha 
-function playAnimation() {
-    $('#flecha').attr('src', 'MEDIA/EXTRA/flecha.gif').css('filter', 'sepia(41%) brightness(64%) hue-rotate(23deg) saturate(84%)');
-  }
-  
-  function showStatic() {
-    $('#flecha').attr('src', 'MEDIA/EXTRA/flecha-estatica.jpg').css('filter', 'none');
-  }
 
-  // menu que se encoge y redondea al hacer scroll
-
-const menu = document.getElementById("menu");
-const navbarCollapse = document.getElementById("navbarNav");
-
-window.addEventListener('scroll', function() {
-    const scrollY = window.scrollY;
- 
-    menu.style.borderRadius = scrollY === 0 ? '0' : '900px';
-
-    if (scrollY > 50) { 
-        menu.classList.add('scrolled');
-    } else {
-        menu.classList.remove('scrolled');
-    }
-});
-
-navbarCollapse.addEventListener('show.bs.collapse', function () {
-    menu.style.borderRadius = "30px";
-});
-
-navbarCollapse.addEventListener('hide.bs.collapse', function () {
-    menu.style.borderRadius = "900px";
-});
-
-
-// scroll indicator
-$(window).scroll(function () {
-  scrollIndicator();
-});
-
-function scrollIndicator() {
-  var winScroll = $(document).scrollTop();
-  var height = $(document).height() - $(window).height();
-  var scrolled = (winScroll / height) * 100;
-  $("#bar").width(scrolled + "%");
-}
-
-
-// menu movil
-const menuItems = document.querySelectorAll('.navbar-nav .nav-item');
-
-menuItems.forEach(item => {
-  item.addEventListener('click', () => {
-    const navbarCollapse = document.querySelector('.navbar-collapse');
-    navbarCollapse.classList.remove('show');
-  });
-});
 
 
 // circle movement
